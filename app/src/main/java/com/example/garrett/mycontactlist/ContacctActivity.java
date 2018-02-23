926aaea1a6a38455c1ea0d126ad557ddf1385afa
Garrett.Voss <bannenbl@uwosh.edu>
2018-02-23 10:17:19 -0600

    Coded intents for the Navbar Image Buttons

--- app/src/main/java/com/example/garrett/mycontactlist/ContacctActivity.java
+++ app/src/main/java/com/example/garrett/mycontactlist/ContacctActivity.java
@@ -1,7 +1,11 @@
 package com.example.garrett.mycontactlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class ContacctActivity extends AppCompatActivity {
 
@@ -9,5 +13,42 @@ public class ContacctActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacct);
+        initListButton(); // initializes the imageButton so that it can be clicked
+        initMapButton();
+        initSettingsButton();
+    }
+
+    private void initListButton() {
+        ImageButton ibList = (ImageButton) findViewById(R.id.ImageButtonList); //create a variable to hold the imageButton object
+        ibList.setOnClickListener(new View.OnClickListener() { // set a listener to wait for a click on the image to occur
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContacctActivity.this, ContactListActivity.class); // sets an intent that comes from ContactActivity, and sends
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // the user to the ContactListActivity, and also clears any pre-existing version of that page
+                startActivity(intent); // executes the intent
+            }
+        });
+    }
+    private void initMapButton() {
+        ImageButton ibMap = (ImageButton) findViewById(R.id.ImageButtonMap); //create a variable to hold the imageButton object
+        ibMap.setOnClickListener(new View.OnClickListener() { // set a listener to wait for a click on the image to occur
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContacctActivity.this, ContactMapActivity.class); // sets an intent that comes from ContactActivity, and sends
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // the user to the ContactListActivity, and also clears any pre-existing version of that page
+                startActivity(intent); // executes the intent
+            }
+        });
+    }
+    private void initSettingsButton() {
+        ImageButton ibSettings = (ImageButton) findViewById(R.id.ImageButtonSettings); //create a variable to hold the imageButton object
+        ibSettings.setOnClickListener(new View.OnClickListener() { // set a listener to wait for a click on the image to occur
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContacctActivity.this, ContactSettingsActivity.class); // sets an intent that comes from ContactActivity, and sends
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // the user to the ContactListActivity, and also clears any pre-existing version of that page
+                startActivity(intent); // executes the intent
+            }
+        });
     }
 }
