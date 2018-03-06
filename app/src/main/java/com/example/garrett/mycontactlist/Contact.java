2e6dee661005bd1ae60a5a3918754b25f4ea28bf
Garrett Voss <garrettvoss4@gmail.com>
2018-03-05 19:35:11 -0600

    Instantiated ContactDataSource Class

---
+++ app/src/main/java/com/example/garrett/mycontactlist/Contact.java
@@ -0,0 +1,103 @@
+package com.example.garrett.mycontactlist;
+
+import java.util.Calendar;
+
+public class Contact {
+
+    private int contactID;
+    private String contactName;
+    private String streetaddress;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String phonenumber;
+    private String cellnumber;
+    private String eMail;
+    private Calendar birthday;
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public void setContactID(int contactID) {
+        this.contactID = contactID;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getStreetaddress() {
+        return streetaddress;
+    }
+
+    public void setStreetaddress(String streetaddress) {
+        this.streetaddress = streetaddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getCellnumber() {
+        return cellnumber;
+    }
+
+    public void setCellnumber(String cellnumber) {
+        this.cellnumber = cellnumber;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    public Contact() {
+        contactID = -1;
+        birthday=Calendar.getInstance();
+
+    }
+}
--- app/src/main/java/com/example/garrett/mycontactlist/ContactDataSource.java
+++ app/src/main/java/com/example/garrett/mycontactlist/ContactDataSource.java
@@ -1,8 +1,23 @@
 package com.example.garrett.mycontactlist;
 
-/**
- * Created by Garrett on 3/5/2018.
- */
+
+import android.content.Context;
+import java.sql.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 
 public class ContactDataSource {
+    private SQLiteDatabase database;
+    private ContactDBHelper dbHelper;
+
+    public ContactDataSource(Context context) {
+        dbHelper = new ContactDBHelper((context));
+    }
+
+    public void open()throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
 }
