6fb5c03affdf4a2f36c7af400fd496e81f82d841
Garrett Voss <garrettvoss4@gmail.com>
2018-03-05 19:02:03 -0600

    Coded functionality of radio buttons and navbar for settings page

--- app/src/main/java/com/example/garrett/mycontactlist/ContactSettingsActivity.java
+++ app/src/main/java/com/example/garrett/mycontactlist/ContactSettingsActivity.java
@@ -1,7 +1,14 @@
 package com.example.garrett.mycontactlist;
 
+import android.content.Context;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ContactSettingsActivity extends AppCompatActivity {
 
@@ -9,5 +16,113 @@ public class ContactSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_settings);
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+        initSettings();
+        initSortByClick();
+        initSortOrderClick();
+    }
+
+    private void initSortOrderClick() {
+        RadioGroup rgSortOrder = (RadioGroup) findViewById(R.id.radioGroupSortOrder);
+        rgSortOrder.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int arg1) {
+                RadioButton rbAscending = (RadioButton) findViewById(R.id.radioAscending);
+
+                if(rbAscending.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).edit().putString("sortorder", "ASC").commit();
+                }
+                else {
+                    getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).edit().putString("sortorder", "DESC").commit();
+                }
+            }
+        });
+    }
+
+    private void initSortByClick() {
+        RadioGroup rgSortBy = (RadioGroup) findViewById(R.id.radioGroupSortBy);
+        rgSortBy.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int arg1) {
+                RadioButton rbName = (RadioButton) findViewById(R.id.radioName);
+                RadioButton rbCity = (RadioButton) findViewById(R.id.radioCity);
+                if(rbName.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).edit().putString("sortfield", "contactname").commit();
+                }
+                else if(rbCity.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).edit().putString("sortfield", "city").commit();
+                }
+                else {
+                    getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).edit().putString("sortfield", "birthday").commit();
+                }
+            }
+        });
+    }
+
+    private void initSettings() {
+        String sortBy = getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).getString("sortfield","contactname");
+        String sortOrder = getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).getString("sortorder", "ASC");
+
+        RadioButton rbName = (RadioButton) findViewById(R.id.radioName);
+        RadioButton rbCity = (RadioButton) findViewById(R.id.radioCity);
+        RadioButton rbBirthday = (RadioButton) findViewById(R.id.radioBirthday);
+        if(sortBy.equalsIgnoreCase("contactname")) {
+            rbName.setChecked(true);
+        }
+        else if (sortBy.equalsIgnoreCase("city")) {
+            rbCity.setChecked(true);
+        }
+        else {
+            rbBirthday.setChecked(true);
+        }
+
+        RadioButton rbAscending = (RadioButton) findViewById(R.id.radioAscending);
+        RadioButton rbDescending = (RadioButton) findViewById(R.id.radioDescending);
+
+        if(sortOrder.equalsIgnoreCase("ASC")) {
+            rbAscending.setChecked(true);
+        }
+        else {
+            rbDescending.setChecked(true);
+        }
+    }
+
+    private void initListButton() {
+        ImageButton ibList = (ImageButton) findViewById(R.id.ImageButtonList); //create a variable to hold the imageButton object
+        ibList.setOnClickListener(new View.OnClickListener() { // set a listener to wait for a click on the image to occur
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactListActivity.class); // sets an intent that comes from ContactActivity, and sends
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // the user to the ContactListActivity, and also clears any pre-existing version of that page
+                startActivity(intent); // executes the intent
+            }
+        });
+    }
+    private void initMapButton() {
+        ImageButton ibMap = (ImageButton) findViewById(R.id.ImageButtonMap); //create a variable to hold the imageButton object
+        ibMap.setOnClickListener(new View.OnClickListener() { // set a listener to wait for a click on the image to occur
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactMapActivity.class); // sets an intent that comes from ContactActivity, and sends
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // the user to the ContactListActivity, and also clears any pre-existing version of that page
+                startActivity(intent); // executes the intent
+            }
+        });
+    }
+    private void initSettingsButton() {
+        ImageButton ibSettings = (ImageButton) findViewById(R.id.ImageButtonSettings); //create a variable to hold the imageButton object
+        ibSettings.setEnabled(false);
+       /* ibSettings.setOnClickListener(new View.OnClickListener() { // set a listener to wait for a click on the image to occur
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactSettingsActivity.class); // sets an intent that comes from ContactActivity, and sends
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // the user to the ContactListActivity, and also clears any pre-existing version of that page
+                startActivity(intent); // executes the intent
+            }*//*
+        });*/
     }
 }
