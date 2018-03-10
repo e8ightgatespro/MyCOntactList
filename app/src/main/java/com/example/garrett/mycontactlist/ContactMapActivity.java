cd3f0fd32f8fd85d3b31bcfe3848b6caa1bf5b59
Garrett Voss <garrettvoss4@gmail.com>
2018-03-10 12:14:50 -0600

    Changed from geocoding to GPS sensor to get coordinates.

--- app/src/main/java/com/example/garrett/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/garrett/mycontactlist/ContactMapActivity.java
@@ -1,8 +1,12 @@
 package com.example.garrett.mycontactlist;
 
+import android.content.Context;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +14,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
 
 public class ContactMapActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    LocationListener gpsListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +33,23 @@ public class ContactMapActivity extends AppCompatActivity {
         initGetLocationButton();
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        try{
+            locationManager.removeUpdates(gpsListener);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void initGetLocationButton() {
         Button locationButton = findViewById(R.id.buttonGetLocation);
         locationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editAddress = findViewById(R.id.editAddress);
+                /*EditText editAddress = findViewById(R.id.editAddress);
                 EditText editCity = findViewById(R.id.editCity);
                 EditText editState = findViewById(R.id.editState);
                 EditText editZipcode = findViewById(R.id.editZipcode);
@@ -54,7 +72,39 @@ public class ContactMapActivity extends AppCompatActivity {
                 TextView txtLongitude = findViewById(R.id.textLongitude);
 
                 txtLatitude.setText("Latitude: " + String.valueOf(addresses.get(0).getLatitude()));
-                txtLongitude.setText("Longitude: " + String.valueOf(addresses.get(0).getLongitude()));
+                txtLongitude.setText("Longitude: " + String.valueOf(addresses.get(0).getLongitude()));*/
+
+                /* the above code is for grabbing coordinates using geocoding and an address */
+
+                try {
+                    locationManager = (LocationManager) getBaseContext().getSystemService(Context.LOCATION_SERVICE);
+                    gpsListener = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            TextView txtLatitude = findViewById(R.id.textLatitude);
+                            TextView txtLongitude = findViewById(R.id.textLongitude);
+                            TextView txtAccuracy = findViewById(R.id.textAccuracy);
+
+                            txtLatitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
+                            txtLongitude.setText("Longitude: " + String.valueOf(location.getLongitude()));
+                            txtAccuracy.setText("Accuracy: " +  String.valueOf(location.getAccuracy()));
+                        }
+
+                        @Override
+                        public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+                        @Override
+                        public void onProviderEnabled(String s) {}
+
+                        @Override
+                        public void onProviderDisabled(String s) {}
+                    };
+
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,gpsListener);
+                }
+                catch(Exception e) {
+                    Toast.makeText(getBaseContext(), "Error, location not available", Toast.LENGTH_LONG).show();
+                }
 
             }
         });
